86b8b7a3e1769e50121e37bb73c12801f41be773
Jeremiah Vaughan <jeremiah.vaughan1@student.ctuonline.edu>
2019-06-01 09:02:06 -0500

    Had an issue with imports on  classes: Blocker, Note, and Project. Correct import has been applied

--- src/main/kotlin/com/madhax/trakker/model/Note.kt
+++ src/main/kotlin/com/madhax/trakker/model/Note.kt
@@ -1,11 +1,12 @@
 package com.madhax.trakker.model
 
-import org.springframework.data.annotation.Id
+
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.OneToOne
+import javax.persistence.*
 
 @Entity
 data class Note(
--- src/main/kotlin/com/madhax/trakker/model/Project.kt
+++ src/main/kotlin/com/madhax/trakker/model/Project.kt
@@ -1,6 +1,6 @@
 package com.madhax.trakker.model
 
-import org.springframework.data.annotation.Id
+
 import javax.persistence.*
 
 @Entity

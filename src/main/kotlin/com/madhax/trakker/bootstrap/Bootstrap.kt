d328c247d841b12bcc95d66ecf2365549753493b
James Cathcart <cathcart.jm@gmail.com>
2019-06-01 23:25:04 -0600

    Added user and project service to the constructor

--- src/main/kotlin/com/madhax/trakker/bootstrap/Bootstrap.kt
+++ src/main/kotlin/com/madhax/trakker/bootstrap/Bootstrap.kt
@@ -1,17 +1,24 @@
 package com.madhax.trakker.bootstrap
 
+import com.madhax.trakker.service.ProjectService
+import com.madhax.trakker.service.UserService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
 @Component
-class Bootstrap : CommandLineRunner {
+class Bootstrap(
+        @Autowired
+        private val userService: UserService,
+        @Autowired
+        private val projectService: ProjectService
+) : CommandLineRunner {
 
     val log: Logger = LoggerFactory.getLogger(Bootstrap::class.java)
 
     override fun run(vararg args: String?) {
         log.debug("I am Bootstrap")
     }
-
 }
